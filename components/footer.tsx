8f0ec71d187f936b1110cdecf5b3ddb9102fe61f
Fernando Bouchet <fernandobouchet@gmail.com>
2025-10-26 23:58:17 -0300

    feat: update 'header' and 'footer'

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,15 @@ const Footer = () => {
     <footer className="border-t mt-16">
       <div className="container mx-auto px-4 py-6">
         <p className="text-center text-sm text-muted-foreground">
-          Datos obtenidos via Telegram bot
+          Datos obtenidos via{" "}
+          <a
+            href="https://github.com/fernandobouchet/job-alert-bot"
+            target="_blank"
+            className="underline"
+          >
+            Job Alert Bot
+          </a>
+          .
         </p>
       </div>
     </footer>
--- components/header.tsx
+++ components/header.tsx
@@ -1,12 +1,10 @@
-import { Briefcase, Calendar } from "lucide-react";
+import { Briefcase } from "lucide-react";
 import { ThemeToggle } from "./theme-toggle";
-import Link from "next/link";
-import { Button } from "./ui/button";
 
 const Header = () => {
   return (
-    <header className="border-b bg-card">
-      <div className="container mx-auto px-4 py-6">
+    <header className="border-b bg-card sticky top-0 z-50">
+      <div className="container mx-auto px-4 py-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-3">
             <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-primary text-primary-foreground">
@@ -22,12 +20,6 @@ const Header = () => {
             </div>
           </div>
           <div className="flex gap-4 items-center">
-            <Link href="/prev_jobs">
-              <Button size="sm" className="gap-2">
-                <Calendar className="h-4 w-4" />
-                Ver Empleos Anteriores
-              </Button>
-            </Link>
             <ThemeToggle />
           </div>
         </div>
